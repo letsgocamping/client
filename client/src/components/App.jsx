f58374016620e5f93f3661c339cfedc87205ef10
Nicole Nguyen <ninguyen00@gmail.com>
2018-09-15 11:45:52 -0500

    Add splash page component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 // import { Layout, Header, HeaderRow, Textfield, Drawer, Content, Navigation } from 'react-mdl'
 // import './App.css';
-import Map from './Map.jsx'
+import Map from './Map.jsx';
+import Splash from './Splash.jsx';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <Map />
-            </div>
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Map />
+        <Splash />
+      </div>
 
-        );
-    }
+    );
+  }
 }
 
 export default App;
