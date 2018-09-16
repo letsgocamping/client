d1eeff28c1feac8e64b9ed1c4d8ad861d138c36e
Elyse Vest <evest90@gmail.com>
2018-09-15 21:02:52 -0500

    do something

--- client/src/components/addTripPage/AddTripPage.jsx
+++ client/src/components/addTripPage/AddTripPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './Header.jsx';
-import Map from '../Map.jsx';
+import Container from './Map/Container.jsx';
 
 class AddTripPage extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <Map />
+        <Container />
       </div>
     );
   }
