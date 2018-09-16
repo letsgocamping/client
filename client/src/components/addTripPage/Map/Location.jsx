999698aa5754229bbf2c57346e96256e8eebfbc7
Jacob Johnston <jacob@johnstonjacob.com>
2018-09-16 03:16:03 -0500

    Update Location.jsx

--- client/src/components/addTripPage/Map/Location.jsx
+++ client/src/components/addTripPage/Map/Location.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { api_url } from '../../../../config.js';
+
 export default class Location extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +11,7 @@ export default class Location extends React.Component {
   render() {
     return (
       <img style={{height: '25px', width: '25px'}}
-        src={'http://localhost:1337/images/Campfire-PNG-Transparent.png'} />
+        src={`${api_url}/images/Campfire-PNG-Transparent.png`} />
     );
   }
 }
