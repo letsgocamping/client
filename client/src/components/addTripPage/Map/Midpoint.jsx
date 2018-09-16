fcf6e376ffc3836092a1a3bd20210673ba65cde5
Alex Donesky <adonesky@gmail.com>
2018-09-16 11:39:01 -0500

    new image for midPoint

---
+++ client/src/components/addTripPage/Map/Midpoint.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MidPoint extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <img style={{height: '25px', width: '25px'}}
+        src={'https://github.com/letsgocamping/client/blob/master/client/dist/assets/Campfire-PNG-Transparent.png?raw=true'} />
+    );
+  }
+}
